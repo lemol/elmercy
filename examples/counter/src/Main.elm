2d8421b660450ce29544465ce1403303c4db1c77
Leza Morais Lutonda <lemol-c@hotmail.com>
2020-02-06 10:53:20 +0000

    add hello_world and counter examples

---
+++ examples/counter/src/Main.elm
@@ -0,0 +1,53 @@
+module Main exposing (main)
+
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { counter : Int
+    }
+
+
+
+-- MESSAGES
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+
+-- UPDATE
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            { model | counter = counter + 1 }
+
+        Decrement ->
+            { model | counter = counter - 1 }
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div
+        [ button
+            [ onClick Decrement ]
+            [ text "-" ]
+        , text (String.fromInt model.counter)
+        , button
+            [ onClick Increment ]
+            [ text "+" ]
+        ]
